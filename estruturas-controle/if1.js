70f2dd0dcb1a60261c737768f4c4d58d6b1a10e7
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2022-07-25 18:08:27 -0300

    Ex 3 - 25/07

---
+++ estruturas-controle/if1.js
@@ -0,0 +1,11 @@
+function Notas(nota){
+    if(nota >= 7){
+        console.log("Aprovado com " + nota)
+    }
+    else{
+        console.log("Reprovado com " + nota)
+    }
+}
+
+Notas(10) // Aprovado com 10
+Notas(5) // Reprovado com 5
---
+++ estruturas-controle/switch.js
@@ -0,0 +1,7 @@
+const resultado = function (nota) {
+    switch (Math.floor(nota)){
+        case 10:
+        case 9:
+            console.log("Notas Máximas!")
+    }
+}
