9583079249f06f5e84c82ebaeb545c1550c0362c
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2022-07-25 20:15:20 -0300

    Ex 3 - 25/07

---
+++ estruturas-controle/forIn.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: 'Will',
+    idade:25,
+    peso:50
+}
+
+for (atributo in pessoa){
+    console.log
+    (`${atributo} = ${pessoa[atributo]}`)
+}
+
+/*
+nome = Will
+idade = 25
+peso = 50
+*/
